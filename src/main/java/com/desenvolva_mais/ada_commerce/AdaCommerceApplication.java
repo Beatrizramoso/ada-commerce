035bb6249e6b318681697160f4dcf1abe4ef54d2
Beatriz <beatrizramoso2024@gmail.com>
2025-08-25 20:46:37 -0300

    Primeiro commit

---
+++ src/main/java/com/desenvolva_mais/ada_commerce/AdaCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolva_mais.ada_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desenvolva_mais/ada_commerce/AdaCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolva_mais.ada_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
