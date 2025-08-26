7c27cc3fd9c5bb6f9842da0ded4294712ae4e5fe
Beatriz <beatrizramoso2024@gmail.com>
2025-08-25 21:11:33 -0300

    Cria métodos para ProdutoService

--- src/main/java/com/desenvolva_mais/ada_commerce/service/ProdutoService.java
+++ src/main/java/com/desenvolva_mais/ada_commerce/service/ProdutoService.java
@@ -1,4 +1,26 @@
 package com.desenvolva_mais.ada_commerce.service;
 
+import com.desenvolva_mais.ada_commerce.model.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProdutoService {
+
+    public Produto cadastrarProduto(Produto produto) {
+        return produto;
+    }
+
+    public Produto atualizarProduto(Produto produto) {
+        return produto;
+    }
+
+    public List<Produto> listarProdutos() {
+        return new ArrayList<>();
+    }
+
+    public Produto buscarProduto(Long id) {
+        return new Produto();
+    }
+
 }
