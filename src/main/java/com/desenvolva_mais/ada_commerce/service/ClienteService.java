755de55fba62c69b7249be8e42fe14b401c4f64e
Beatriz <beatrizramoso2024@gmail.com>
2025-08-25 21:11:17 -0300

    Cria métodos para ClienteService

--- src/main/java/com/desenvolva_mais/ada_commerce/service/ClienteService.java
+++ src/main/java/com/desenvolva_mais/ada_commerce/service/ClienteService.java
@@ -1,4 +1,26 @@
 package com.desenvolva_mais.ada_commerce.service;
 
+import com.desenvolva_mais.ada_commerce.model.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteService {
+
+    public Cliente cadastrarCliente(Cliente cliente) {
+        return cliente;
+    }
+
+    public Cliente atualizarCliente(Cliente cliente) {
+        return cliente;
+    }
+
+    public List<Cliente> listarClientes() {
+        return new ArrayList<>();
+    }
+
+    public Cliente buscarCliente(String documento) {
+        return new Cliente();
+    }
+
 }
