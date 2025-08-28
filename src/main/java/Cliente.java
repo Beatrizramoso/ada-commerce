d77329465311d2b53e0343df074f80af625b2de9
Nathalia Soares <taiapeixoto@gmail.com>
2025-08-28 13:06:22 -0300

    Definicao dos atributos da classe Cliente

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -2,4 +2,8 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Cliente {
+    private final UUID id;
+    private String nome;
+    private String documento;
+    private String email;
 }
