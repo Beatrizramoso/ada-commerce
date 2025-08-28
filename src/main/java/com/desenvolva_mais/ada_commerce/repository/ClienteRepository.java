61200682f088a23c099654207f1e8e6540ce0372
Beatriz <beatrizramoso2024@gmail.com>
2025-08-28 17:31:33 -0300

    Cria ClienteRepository e implementa no ClienteService

---
+++ src/main/java/com/desenvolva_mais/ada_commerce/repository/ClienteRepository.java
@@ -0,0 +1,14 @@
+package com.desenvolva_mais.ada_commerce.repository;
+
+import com.desenvolva_mais.ada_commerce.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+
+    Cliente findByDocumento(String documento);
+
+}
--- src/main/java/com/desenvolva_mais/ada_commerce/service/ClienteService.java
+++ src/main/java/com/desenvolva_mais/ada_commerce/service/ClienteService.java
@@ -1,26 +1,32 @@
 package com.desenvolva_mais.ada_commerce.service;
 
 import com.desenvolva_mais.ada_commerce.model.Cliente;
+import com.desenvolva_mais.ada_commerce.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class ClienteService {
 
+    private final ClienteRepository repository;
+
     public Cliente cadastrarCliente(Cliente cliente) {
-        return cliente;
+        return repository.save(cliente);
     }
 
     public Cliente atualizarCliente(Cliente cliente) {
-        return cliente;
+        return repository.save(cliente);
     }
 
     public List<Cliente> listarClientes() {
-        return new ArrayList<>();
+        return repository.findAll();
     }
 
     public Cliente buscarCliente(String documento) {
-        return new Cliente();
+        return repository.findByDocumento(documento);
     }
 
 }
