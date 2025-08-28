aead43900fb9c26f960a8503f21e368334ca024f
Nathalia Soares <taiapeixoto@gmail.com>
2025-08-28 13:10:20 -0300

    Definicao dos construtores da classe Cliente

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -6,4 +6,11 @@ public class Cliente {
     private String nome;
     private String documento;
     private String email;
+
+    public Cliente(String nome, String documento, String email) {
+        this.id = UUID.randomUUID();
+        this.nome = nome;
+        this.documento = documento;
+        this.email = email;
+    }
 }
