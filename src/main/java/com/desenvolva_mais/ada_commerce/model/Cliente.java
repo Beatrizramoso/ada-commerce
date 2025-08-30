2876d9cea3d805357a463e17f55584425cb6141b
Beatriz <beatrizramoso2024@gmail.com>
2025-08-30 10:33:37 -0300

    Adiciona anotação de Entity

--- src/main/java/com/desenvolva_mais/ada_commerce/model/Cliente.java
+++ src/main/java/com/desenvolva_mais/ada_commerce/model/Cliente.java
@@ -1,5 +1,6 @@
 package com.desenvolva_mais.ada_commerce.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Cliente {
 
     @Id
--- src/main/java/com/desenvolva_mais/ada_commerce/model/ItemPedido.java
+++ src/main/java/com/desenvolva_mais/ada_commerce/model/ItemPedido.java
@@ -1,5 +1,6 @@
 package com.desenvolva_mais.ada_commerce.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class ItemPedido {
 
     @Id
