2b69cbc85e6e4a42893ec4c7706e1c9de5e9e543
Schionatto1991 <kschionatto@gmail.com>
2025-08-31 22:09:40 -0300

    Atualizações nas classes ItemPedido, Pedido e StatusPedido

--- src/main/java/com/desenvolva_mais/ada_commerce/model/enums/StatusPedido.java
+++ src/main/java/com/desenvolva_mais/ada_commerce/model/enums/StatusPedido.java
@@ -1,7 +1,8 @@
-package com.desenvolva_mais.ada_commerce.model.enums;
+package com.desenvolva_mais.ada_commerce.model;
 
 public enum StatusPedido {
     ABERTO,
     AGUARDANDO_PAGAMENTO,
+    PAGO,
     FINALIZADO
 }
